197490eaad3f6ab6477e2883a678c03c00e96061
Tusar Ranjan Pati <sipucse@gmail.com>
2021-12-27 10:14:58 +0530

    Update RoleNameEnum.java

--- happiestlearning/src/main/java/com/happiestlearning/constants/RoleNameEnum.java
+++ happiestlearning/src/main/java/com/happiestlearning/constants/RoleNameEnum.java
@@ -1,12 +1,6 @@
 package com.happiestlearning.constants;
 
-/**
- * This Enum holds the different Roles MDI maintains
- *
- * @author TAB
- *
- */
 public enum RoleNameEnum {
 
-    SUPER_ADMIN, SYSTEM_ADMIN, PROJECT_ADMIN, PROJECT_USER, CREATED_USER, DEFAULT_USER, EXCLUDE, REQUEST_PROCESSOR, RESTORE_EXECUTOR;
+    SYSTEM_ADMIN;
 }
